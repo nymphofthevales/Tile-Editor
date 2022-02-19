021fa4ec35fa0b6c3f893b45965b5be874e5674a
nymphofthevales <81981344+nymphofthevales@users.noreply.github.com>
2022-02-19 03:49:00 -0800

    add zoom functionality with classes
    Works, but produces bad performance on large grids due to classList.add(size) iteration over cells and rows; needs to dynamically modify stylesheet instead.

--- public/application.ts
+++ public/application.ts
@@ -2,30 +2,6 @@ import { Grid } from "./scripts/grid.js"
 import { GridSelector } from  "./scripts/selector.js"
 import { GridRenderer, getCellReference } from "./scripts/renderer.js"
 
-let actionModes = {
-    select_deselect: function([x,y]) {
-        if (workingSelector.selection.hasCell([x,y])) {
-            workingSelector.deselect([x,y])
-        } else {
-            workingSelector.select([x,y])
-        }
-    },
-    draw_tiles: function([x,y]) {
-
-    },
-    drag_select: function([x1,y1], [x2,y2]) {
-
-    }
-}
-
-let workingGrid: Grid
-let workingSelector: GridSelector
-let target = document.getElementById('grid-mount')
-let workingRenderer = new GridRenderer(target)
-let ident = workingRenderer.identifier
-
-let currentActionMode = actionModes.select_deselect
-
 class Stack {
     _stack = []
     constuctor() {
@@ -50,7 +26,6 @@ class Stack {
         return this._stack[0]
     }
 }
-
 class Form {
     _housing: HTMLElement
     inputs = {}
@@ -88,6 +63,97 @@ class Form {
         this._close.addEventListener("mouseup", callback) 
     }
 }
+
+let actionModes = {
+    select_deselect: function([x,y]) {
+        if (workingSelector.selection.hasCell([x,y])) {
+            workingSelector.deselect([x,y])
+        } else {
+            workingSelector.select([x,y])
+        }
+    },
+    draw_tiles: function([x,y]) {
+
+    },
+    drag_select: function([x1,y1], [x2,y2]) {
+
+    }
+}
+let keyboardActions  = {
+    zoomin: () => {
+        console.log('zoomin')
+        if (zoomSize != 20) {
+            zoomSize += 1
+            adjustZoom("grid-row")
+            adjustZoom("grid-cell")
+            prevZoom += 1
+        }
+    },
+    zoomout: () => {
+        console.log('zoomout')
+        if (zoomSize != 1) {
+            zoomSize -= 1
+            adjustZoom("grid-row")
+            adjustZoom("grid-cell")
+            prevZoom -= 1
+        }
+    },
+}
+
+function clearSizeClasses(element: Element): void {
+    let height = `grid-h-${prevZoom}`
+    let width = `grid-w-${prevZoom}`
+    element.classList.remove(height)
+    element.classList.remove(width)
+}
+
+function adjustZoom(className: string) {
+    let elements = document.getElementsByClassName(className)
+    for (let i=0; i < elements.length; i++) {
+        clearSizeClasses(elements[i])
+        elements[i].classList.add(`grid-h-${zoomSize}`)
+        if (className != "grid-row") {
+            elements[i].classList.add(`grid-w-${zoomSize}`)
+        }
+    }
+}
+
+let cellInteractions = {
+    click: () => {
+        console.log(mousePosition._stack)
+        let [x,y] = mousePosition.latest
+        currentActionMode([x,y])
+        workingRenderer.renderSelection(workingSelector)
+    },
+    release: () => {
+        console.log('release', mousePosition.latest, mousePosition.oldest)
+    },
+    hover: () => {
+        let [x,y] = mousePosition.latest
+        console.log('hover', mousePosition.latest)
+        getCellReference([x,y], ident).classList.add('hovered')
+    },
+    clearHover: () => {
+        let [x,y] = mousePosition.oldest
+        console.log('clearhover', mousePosition.oldest)
+        getCellReference([x,y], ident).classList.remove('hovered')
+   },
+}
+
+let workingGrid: Grid
+let workingSelector: GridSelector
+let target = document.getElementById('grid-mount')
+let workingRenderer = new GridRenderer(target)
+let ident = workingRenderer.identifier
+let editorOpen = false;
+let zoomSize = 5;
+let prevZoom = 5;
+
+let currentActionMode = actionModes.select_deselect
+
+let mousePosition = new Stack()
+mousePosition['listening'] = false
+
 let setupForm = new Form("map-setup-frame")
 setupForm.addInput("width", "NEWMAP-WIDTH")
 setupForm.addInput("height", "NEWMAP-HEIGHT")
@@ -110,12 +176,18 @@ setupForm.closeAction = () => {
     setupForm.clearInputs()
 }
 
-
 function startEditor() {
+    editorOpen = true;
     workingRenderer.dynamicRender(workingGrid, workingSelector)
     setupCellListeners()
+    setDefaultZoom()
     document.getElementById("main-menu").classList.add("hidden")
     document.getElementById("application-frame").classList.remove("hidden")
+}
+
+function setDefaultZoom() {
+    adjustZoom("grid-row")
+    adjustZoom("grid-cell")
 }
 
 function setupCellListeners() {
@@ -124,64 +196,58 @@ function setupCellListeners() {
         let identifier = workingRenderer.identifier
         let reference = getCellReference( [x,y], identifier )
         reference.addEventListener("pointerenter", () => {
-            if (!mousepos['listening']) {
-                mousepos.clear()
-                mousepos.push([x,y])
+            if (!mousePosition['listening']) {
+                mousePosition.clear()
+                mousePosition.push([x,y])
                 cellInteractions.hover()
             }
         })
         reference.addEventListener("pointerleave", () => {
-            if (!mousepos['listening']) {
+            if (!mousePosition['listening']) {
                 cellInteractions.clearHover()
-                mousepos.clear()
+                mousePosition.clear()
             }
         })
         reference.addEventListener("mousedown", () => {
             cellInteractions.click()
-            mousepos['listening'] = true
+            mousePosition['listening'] = true
         })
         reference.addEventListener("mouseup", () => {
-            mousepos.push([x,y])
+            mousePosition.push([x,y])
             cellInteractions.release()
-            mousepos['listening'] = false
+            mousePosition['listening'] = false
             cellInteractions.clearHover()
-            mousepos.clear()
-            mousepos.push([x,y])
+            mousePosition.clear()
+            mousePosition.push([x,y])
             cellInteractions.hover()
         })
     })  
 }
 
-let mousepos = new Stack()
-mousepos['listening'] = false
+window.addEventListener('keyup', (event) => {
+    let key = event.key
+    if (editorOpen == true) {
+        switch (key) {
+            case '=':
+            case '+':
+                keyboardActions.zoomin()
+                break;
+            case '-':
+            case '_':
+                keyboardActions.zoomout()
+                break;
+        }
+    }
+})
 
-let cellInteractions = {
-    click: () => {
-        console.log(mousepos._stack)
-        let [x,y] = mousepos.latest
-        currentActionMode([x,y])
-        workingRenderer.renderSelection(workingSelector)
-    },
-    release: () => {
-        console.log('release', mousepos.latest, mousepos.oldest)
-    },
-    hover: () => {
-        let [x,y] = mousepos.latest
-        console.log('hover', mousepos.latest)
-        getCellReference([x,y], ident).classList.add('hovered')
-    },
-    clearHover: () => {
-        let [x,y] = mousepos.oldest
-        console.log('clearhover', mousepos.oldest)
-        getCellReference([x,y], ident).classList.remove('hovered')
-   },
-}
 
 
 document.getElementById("MENU-NEW").addEventListener('mouseup', ()=>{
     setupForm.clearInputs()
     setupForm._housing.classList.remove("hidden")
 })
+
+
 
 document.getElementById("CTRL-BASIC-MOVE").addEventListener('mouseup', () => {
 
